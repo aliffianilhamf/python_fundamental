8fa73c687440bf3b23159ca382fa5509d81ff22d
aliffianilhamf <aliffianilhamf@gmail.com>
2023-09-18 20:24:25 +0700

    Add variable folder

---
+++ variable/Main.py
@@ -0,0 +1,16 @@
+# variable adalah tempat untuk menyimpan data
+
+a = 5
+x = 10
+
+#memanggil variable
+print("Ini adalah nilai a = ", a)
+print("Ini adalah nilai x = ", x)
+
+#penamaan
+nilai_y = 15
+huruf6 = 6
+nilaiZ= 7
+
+print("Ini adalah nilai nilai_y = ", nilai_y)
+print("Ini adalah nilai z = ", nilaiZ)
