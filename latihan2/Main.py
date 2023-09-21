e0893d9701b9737d2ba94860db213e900468a469
aliffianilhamf <aliffianilhamf@gmail.com>
2023-09-21 13:14:19 +0700

    add latihan2

---
+++ latihan2/Main.py
@@ -0,0 +1,20 @@
+# Membuat Program Konversi suhu
+
+celcius = float(input("Masukkan suhu celcius : "))
+print("suhu yang anda masukkan adalah ", celcius, "celcius")
+
+
+# reamur
+reamur = (4/5)*celcius
+print("suhu dalam reamur adalah : ",reamur)
+
+# fahrenheit
+fahrenheit = ((9/5)* celcius) + 32
+print("suhu dalam fahrenheit adalah : ",fahrenheit)
+
+# kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah : ",kelvin)
+
+#fahrenheti ke kelvin
+#kelvin ke fahrenheit
