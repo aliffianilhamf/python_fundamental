65df296098ecb48e566a640944a0ab3dd184e868
aliffianilhamf <aliffianilhamf@gmail.com>
2023-09-27 17:14:57 +0700

    Add opertorAssignment folder

---
+++ operator_asignment/Main.py
@@ -0,0 +1,56 @@
+#operator asignment
+# operator di tambah dengan asignment
+
+a = 5 # adalah asignment
+print("nilai a : ", a)
+
+a += 1
+print("nilai a += 1, nilai a menjadi ",a)
+
+a -= 2
+print("nilai a -= 2, nilainya menjadi ", a)
+
+a *= 5
+print("nilai a *= 5, nilainya menjadi ", a)
+
+a /= 2
+print("nilai a /= 2, nilainya menjadi ", a)
+
+
+b = 10
+print("\nilai b = ",b)
+
+b %= 3
+print("nilai b %= 3, nilainya menjadi ", b) 
+
+b = 10
+print("\nilai b = ",b)
+
+b //= 3
+print("nilai b %= 3, nilainya menjadi ", b) 
+
+a = 5
+print("\nilai a = ",a)
+
+a **= 3
+print("nilai a **= 3, nilainya menjadi ", a) 
+
+# bitwise
+c = False
+print("\nilai c = ",c)
+c |= False
+print("nilai c, |= 3, nilainya menjadi ", c)
+c = False
+print("\nilai c = ",c)
+c |= True
+print("nilai c, |= 3, nilainya menjadi ", c)
+
+c = True
+print("\nilai c = ",c)
+c &= False
+print("nilai c, &= 3, nilainya menjadi ", c)
+c = True
+print("\nilai c = ",c)
+c &= True
+print("nilai c, &= 3, nilainya menjadi ", c)
+
