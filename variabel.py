803b34a6733cd2dc6875196e5f3d54e33206861b
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-19 10:35:06 +0700

    variable in python

---
+++ variabel.py
@@ -0,0 +1,19 @@
+# Variabel adalah tempat untuk menampung data
+
+# assignment nilai
+x = 30
+y = 20
+
+print("nilai x = ", x)
+print("nilai y = ", y)
+
+#penamaan 
+nilai_1 = 5
+nilai_2 = 10
+total_nilai = nilai_1 + nilai_2
+
+print(total_nilai)
+
+#assignment indirect (tidak langsung)
+hasil = total_nilai
+print(hasil)
