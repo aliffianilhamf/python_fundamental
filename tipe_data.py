2fbf0127a297dec7d01ade7f40e44c15d96a0145
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-19 11:28:51 +0700

    tipe data pada python

---
+++ tipe_data.py
@@ -0,0 +1,44 @@
+#Tipe Data Numerik 
+
+# Integer : bilangan bulat (int)
+angka_1 = 1
+
+print("angka 1 : ", angka_1, "memiliki tipe : ", type(angka_1))
+
+# Float : Bilangan desimal / yang ada koma - koma nya (float)
+float_1 = 1.86
+print("angka 1 : ", float_1, "memiliki tipe : ", type(float_1))
+
+# Tipe Data String
+
+# kumpulan karakter (string)
+nama = "joko"
+print("angka 1 : ", nama, "memiliki tipe : ", type(nama))
+
+#Tipe Data Boolean
+
+# True / False (bool)
+bool_1 = False
+print("angka 1 : ", bool_1, "memiliki tipe : ", type(bool_1))
+
+
+
+
+
+# Tipe data Khusus
+# kombinasi bilangan real dan imajiner	(complex)
+complex_1 = complex(1,2)
+print("angka 1 : ", complex_1, "memiliki tipe : ", type(complex_1))
+
+
+#Representasi nilai kosong null atau nill (None)
+none_1 = None
+print("angka 1 : ", none_1, "memiliki tipe : ", type(none_1))
+
+
+#Tipe data dari library bahasa C
+from ctypes import c_double
+double_1 = c_double(10.98)
+print("angka 1 : ", double_1, "memiliki tipe : ", type(double_1))
+
+
