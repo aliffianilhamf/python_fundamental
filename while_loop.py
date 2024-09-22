d34760862f472236671179c5c2146ec21551b377
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-22 10:53:23 +0700

    while loop pada python

---
+++ while_loop.py
@@ -0,0 +1,17 @@
+# While Loop
+
+# while kondisi : 
+#     aksi 1
+#     aksi 2
+    
+# akhir program
+
+angka = 0 
+print(f'Angka sekarang : {angka} \n')
+
+while angka < 7 : 
+    angka += 1
+    print(f'Angka sekarang : {angka} ')
+    print('YEEEY BERHASIL!!')
+    
+print('Akhir program')
