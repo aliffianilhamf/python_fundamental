a63f4de800b1aa7a135f415f4d8257a98f1f1fd1
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-22 09:12:46 +0700

    elif dalam python

---
+++ elif.py
@@ -0,0 +1,15 @@
+# ELIF = else if
+
+nama = input("masukkan nama kamu : ")
+
+if nama == 'alip' : # kondisi 1
+    print('kamu dapet mobill') #Aksi true 1
+elif nama == 'ilham' : # kondisi 2
+    print('kamu dapet bus')#Aksi true 2
+elif nama == 'nando' : # kondisi 3
+    print('kamu dapat pesawat')#Aksi true 3
+else :
+    print('maaf aku ga kenal')#Aksi false
+    
+# akhir program diluar block if
+print('thx uuuu')
