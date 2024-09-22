ad426bb9809c9478eae6599e5f7941760ddf41b0
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-22 10:41:37 +0700

    for loop pada python

---
+++ for_loop.py
@@ -0,0 +1,27 @@
+# Perulangan
+
+## 1. Menggunakan List
+data_list = [1,2,3,4,534,3]
+print(f'List : ', data_list)
+
+for i in data_list : 
+    print(f'i isinya : {i}')
+    
+print('Akhir dari pogram 1 \n')
+
+## 2. Menggunakan Range
+for i in range(7) : 
+    print(f'i isinya : {i}')
+
+print('\n')
+for i in range(4,10):
+    print(f'i isinya : {i}')
+
+print('Akhir dari pogram 2 \n')
+
+## 3. Menggunakan string
+data_str = 'aliffian ilham febriyana'
+
+for huruf in data_str : 
+    print(huruf)
+print('Akhir dari pogram 3 \n')
