9e9684f3e5796f857e15d7d1ca36f128c3cde1d7
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-22 11:17:20 +0700

    break pada python

---
+++ break.py
@@ -0,0 +1,30 @@
+# Break (Berhenti)
+
+# angka = 0
+# print(f'angka sekarang : {angka}\n')
+
+
+# while angka < 5 : 
+#     angka += 1
+#     print(f'angka sekarang : {angka}')
+#     if angka == 3 : 
+#         print('break')
+#         break
+#     print('mantab')
+
+# print('sudahhh cukupp')
+
+data = int(input("Hitung sampai = "))
+
+angka = 0
+
+while True : 
+    angka += 1
+    print(f'count : {angka}')
+    if angka == data : 
+        print('hitung selesai')
+        break
+    print('xixixixixix')
+    
+print('cukuppp sudahhh')
+    
