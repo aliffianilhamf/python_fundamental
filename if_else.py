0be7e0e9489972c044a76af390881a0badf67231
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-22 09:00:37 +0700

    if else pada python

---
+++ if_else.py
@@ -0,0 +1,28 @@
+# if Else statement (Pengkondisian)
+
+nama = input("Masukkan nama anda : ")
+
+# # if mode inline
+# if nama == 'alip' : print(f'hii {nama}, selamat pagi!!')
+
+# ## akhir dari program, diluar block if
+# print(f'terima kasih, {nama}')
+
+
+# # if mode indentation
+# if nama == 'ilham' : 
+#     print('Selamat kamu mendapatkan mobil')
+#     print('yeeeey')
+
+# #  akhir dari program, diluar block if
+# print(f'terima kasih, {nama}')
+
+
+# Else (kondisi jika tidak terpenuhi)
+if nama == 'nando' : 
+    print('Selamat kamu dapet mobil!!')
+    print('jangan lupa bayar pajak ya')
+else : 
+    print('maaf anda kurang beruntung')
+    
+print(f'terima kasih {nama}')
