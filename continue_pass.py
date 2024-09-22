04df63aa76fd22224f4ecf37bd1d090f52ef0294
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-22 11:08:06 +0700

    continue & pass pada python

---
+++ continue_pass.py
@@ -0,0 +1,27 @@
+# PASS 
+# angka = 0
+# print(f'Angka sekarang : {angka}\n')
+
+# while angka < 5 : 
+#     angka += 1
+#     print(f'Angka sekarang : {angka}')
+#     if angka == 3 : 
+#         print('persiapan pass')
+#         pass # tidak akan terjadi apa - apa, dummy saja
+#     print('Mantabb dahhh')
+    
+# print('Cukuuuup')
+
+# CONTINUE
+angka = 0
+print(f'Angka sekarang : {angka}\n')
+
+while angka < 5 : 
+    angka += 1
+    print(f'Angka sekarang : {angka}')
+    if angka == 3 : 
+        print('persiapan continue')
+        continue #akan langsung kembali ke atas (perulangan) kode di bwhnya tidak di eksekusi
+    print('Mantabb dahhh')
+    
+print('Cukuuuup')
