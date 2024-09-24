95f8a90b826fd441e316bd7d6f2662c931d6fb36
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-24 09:22:05 +0700

    latihan dictionary part 2

--- latihan_7.py
+++ latihan_7.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import os
-
-
+import random
+import string
 # template dict mahasiswa
 mhs_template = {
     'nama' : 'jawil jangkung',
@@ -13,15 +13,36 @@ mhs_template = {
 
 os.system("cls")
 data_mhs = {}
-mahasiswa = dict.fromkeys(mhs_template.keys())
-print(10*'=' + 'SELAMAT DATANG'+ 10*'=')
-mahasiswa['nama'] = input("Masukkan Nama mhs : ")
-mahasiswa['nim'] = input("Masukkan NIM mhs : ")
-mahasiswa['sks_lulus'] = int(input("Masukkan SKS mhs : "))
-mahasiswa['beasiswa'] = bool(input('Masukkan beasiswa mhs (True / False) : '))
-TAHUN_LAHIR = int(input("Masukkan tahun lahir (YYYY) : "))
-BULAN_LAHIR = int(input("Masukkan bulan lahir (1-12) : "))
-TANGGAL_LAHIR = int(input("Masukkan tanggal lahir (1-31) : "))
-mahasiswa['lahir'] = dt.datetime(TAHUN_LAHIR, BULAN_LAHIR, TANGGAL_LAHIR)
 
-print(mahasiswa)
+
+while True : 
+    print(10*'=' + 'SELAMAT DATANG'+ 10*'=')
+    mahasiswa = dict.fromkeys(mhs_template.keys())
+    mahasiswa['nama'] = input("Masukkan Nama mhs : ")
+    mahasiswa['nim'] = input("Masukkan NIM mhs : ")
+    mahasiswa['sks_lulus'] = int(input("Masukkan SKS mhs : "))
+    mahasiswa['beasiswa'] = bool(input('Masukkan beasiswa mhs (True / False) : '))
+    TAHUN_LAHIR = int(input("Masukkan tahun lahir (YYYY) : "))
+    BULAN_LAHIR = int(input("Masukkan bulan lahir (1-12) : "))
+    TANGGAL_LAHIR = int(input("Masukkan tanggal lahir (1-31) : "))
+    mahasiswa['lahir'] = dt.datetime(TAHUN_LAHIR, BULAN_LAHIR, TANGGAL_LAHIR)
+
+    KEY = ''.join((random.choice(string.ascii_uppercase) for i in range(6)))
+    data_mhs.update({KEY : mahasiswa})
+    
+    print(f"{'KEY':<6} {'NAMA':<17} {'NIM':<13} {'SKS':<3} {'BEASISWA':<8} {'LAHIR':<8}")
+    print(60*'=')
+    for key in data_mhs:
+        KEY = key
+        NAMA = data_mhs[key]['nama']
+        NIM = data_mhs[key]['nim']
+        SKS = data_mhs[key]['sks_lulus']
+        BEASISWA = data_mhs[key]['beasiswa']
+        LAHIR = data_mhs[key]['lahir'].strftime("%x")
+        
+        print(f"{KEY:<6} {NAMA:<17} {NIM:<13} {SKS:<3} {BEASISWA:^8} {LAHIR:<8}")
+    is_lanjut = input("Lanjut (y/n?")
+    if is_lanjut == 'n' : 
+        break
+
+print("TERIMA KASIH")
