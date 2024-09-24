850a4e9b3dcc6d9a013c2f70af345af5cfc55ffe
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-24 11:29:54 +0700

    argument dan parameter

---
+++ parameter_argument_function.py
@@ -0,0 +1,34 @@
+# Argument dan parameter
+def tambah(angka1, angka2):
+    print(f'{angka1} + {angka2} = {angka1+angka2}')
+    
+    
+    
+tambah(1,2)
+tambah(45,67)
+
+
+def say_hii(data):
+    list_data = data.copy()
+    list_data[0]='farhan'
+    for nama in list_data : 
+        print(f'Haiiii {nama}')
+
+
+data_list = ['alip','nando']
+say_hii(data_list)
+
+print(data_list)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
