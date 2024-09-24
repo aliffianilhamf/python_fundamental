d2f69c576fdac6a9fba45de893aa6784fe0424b5
aliffianilhamf <aliffianilhamf@gmail.com>
2024-09-24 10:49:24 +0700

    pengenalan fungsi

---
+++ function.py
@@ -0,0 +1,12 @@
+# Fungsi
+def hello_world(): 
+    print('Hello world!')
+    
+hello_world()
+
+# fungsi_tambah() # error
+def fungsi_tambah():
+    print(1+1)
+    
+# manggilnya setelah fungsi di deklarasi
+fungsi_tambah()
